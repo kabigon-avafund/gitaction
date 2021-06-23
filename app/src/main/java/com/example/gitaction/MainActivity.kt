2a9fdf4ec92ac52f49e5e339662338a9bd3da288
Jakrapun Cholchalatharn <jakrapun@ava.fund>
2021-06-23 14:08:37 +0700

    commit #1

--- app/src/main/java/com/example/gitaction/MainActivity.kt
+++ app/src/main/java/com/example/gitaction/MainActivity.kt
@@ -8,7 +8,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        a()
-        b()
+        home()
+        home1()
     }
 }
